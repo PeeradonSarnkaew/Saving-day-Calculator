075e9220c7d7430191c0129d1be7a74cb8a52e9d
PeeradonSarnkaew <68162914+PeeradonSarnkaew@users.noreply.github.com>
2021-07-28 17:28:11 +0700

    Adding Files
    เพิ่มไฟล์สำหรับโปรแกรม

---
+++ test01/test01/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void wantText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Cal(object sender, RoutedEventArgs e)
+        {
+            float income = float.Parse(incomeText.Text);
+            float pay = float.Parse(payText.Text);
+            float want = float.Parse(wantText.Text);
+            int day = (int)(Math.Ceiling(want / (income - pay)));
+            MessageBox.Show("การคำนวณเสร็จสิ้น");
+            displayText.Text = day.ToString();
+        }
+    }
+}
